83eab221319614c835101a63121abde881612788
mikaelsonbraz <mikaelsonmrn@gmail.com>
2021-11-30 15:02:55 -0300

    startapp pages

---
+++ pages/views.py
@@ -0,0 +1,9 @@
+from django.views.generic import TemplateView
+
+
+class AboutPageView(TemplateView):
+    template_name = "about.html"
+
+
+class HomePageView(TemplateView):
+    template_name = "home.html"
