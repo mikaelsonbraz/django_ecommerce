09b2425cabfebef93ce01b5aca3a638794c908eb
mikaelsonbraz <mikaelsonmrn@gmail.com>
2021-12-01 11:13:03 -0300

    urls da raiz

--- django_ecommerce/urls.py
+++ django_ecommerce/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("pages.urls")),
+    path("products/", include("products.urls"))
 ]
 
 if settings.DEBUG == True:
